f370003557a0f77e4a342788fd35a723bf28a687
harsh2002 <harsh.ind121@gmail.com>
2024-06-14 23:42:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
-provider "aws" {
-  region  = "us-east-1"
-  access_key = ""
-  secret_key = ""
+terraform {
+  required_providers {
+    conjur = {
+      source = "cyberark/conjur"
+      version = "0.6.3"
+    }
+  }
 }
